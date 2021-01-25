43f81c8432bc4593d1c1aa22c6798765f806f6be
ElisabethDuong <elisabeth.duong@gmail.com>
2021-01-26 00:57:40 +0100

    elsy steps 3-5

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import './App.css';
+import Box from "./components/Box";
+import "./styles/global.css";
+import "./App.css";
 
 const tempMin = -20;
 const tempMax = 40;
@@ -8,14 +10,40 @@ const heartMax = 180;
 const stepsMin = 0;
 const stepsMax = 50000;
 
-export class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
-        <p>Heart: {heartMin}</p>
-        <p>Temperature: {tempMin}</p>
-        <p>Steps: {stepsMin}</p>
+
+          {/* Water */}
+          <Box icon="local_drink" 
+            color="#3A85FF" 
+            value={1.5} 
+            unit="L" />
+
+          {/* Steps */}
+          <Box icon="directions_walk" 
+            color="black" 
+            value={3000} 
+            unit="steps" />
+
+          {/* Heart */}
+          <Box icon="favorite" 
+            color="red" 
+            value={120} 
+            unit="bpm" />
+
+          {/* Temperature */}
+          <Box icon="wb_sunny" 
+            color="yellow" 
+            value={-10} 
+            unit="°C" />
+
+          {/* <p>Heart: {heartMin}</p>
+          <p>Temperature: {tempMin}</p>
+          <p>Steps: {stepsMin}</p> */}
+          
         </div>
       </div>
     );
