4a4505ee354b769a6686765a9e925b5d7767e3d6
ElisabethDuong <elisabeth.duong@gmail.com>
2021-01-26 15:32:44 +0100

    elsy step 6

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -11,39 +11,49 @@ const stepsMin = 0;
 const stepsMax = 50000;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    };
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
 
           {/* Water */}
-          <Box icon="local_drink" 
-            color="#3A85FF" 
-            value={1.5} 
+          <Box icon="local_drink"
+            color="#3A85FF"
+            value={1.5}
             unit="L" />
 
           {/* Steps */}
-          <Box icon="directions_walk" 
-            color="black" 
-            value={3000} 
+          <Box icon="directions_walk"
+            color="black"
+            value={3000}
             unit="steps" />
 
           {/* Heart */}
-          <Box icon="favorite" 
-            color="red" 
-            value={120} 
+          <Box icon="favorite"
+            color="red"
+            value={120}
             unit="bpm" />
 
           {/* Temperature */}
-          <Box icon="wb_sunny" 
-            color="yellow" 
-            value={-10} 
+          <Box icon="wb_sunny"
+            color="yellow"
+            value={-10}
             unit="°C" />
 
           {/* <p>Heart: {heartMin}</p>
           <p>Temperature: {tempMin}</p>
           <p>Steps: {stepsMin}</p> */}
-          
+
         </div>
       </div>
     );
--- elsy/src/components/Box.jsx
+++ elsy/src/components/Box.jsx
@@ -4,10 +4,11 @@ class Box extends React.Component {
     render() {
         return (
             <div className="box col-sm-3 col-6">
-                <span className="material-icons" style={{fontSize: 100, color: this.props.color}}>
+                <span className="material-icons" style={{ fontSize: 100, color: this.props.color }}>
                     {this.props.icon}
                 </span>
                 <p>{this.props.value} {this.props.unit}</p>
+                <input type="range" min="" max="" value="" onInput="" />
             </div>
         )
     };
