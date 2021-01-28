ae86769209c73c5a54c27fd0c932db8437074990
ElisabethDuong <elisabeth.duong@gmail.com>
2021-01-28 10:55:26 +0100

    partial revision

--- counter/src/App.js
+++ counter/src/App.js
@@ -13,11 +13,30 @@ class App extends React.Component {
     this.generateAlert = this.generateAlert.bind(this);
   }
 
+  // On peut bind directement dans les button mais ça ne marche que si la fonction n'est utilisée qu'une seule fois !
+  // Autrement il vaut mieux les bind dans le constructor. Là elles pourront être réutilisées dans toute l'application.
+
   decrementCount() {
     this.setState({
       count: this.state.count - 1
     });
   }
+
+  // Méthode avec fonction fléchée
+  // decrementCount = () => {
+  //   this.setState({ count: this.state.count - 1 })
+  // }
+
+  // Autre méthode pour que le counter soit toujours > 0
+  // decrementCount = () => {
+  //   let result = this.state.count;
+  //   if (result === 0) {
+  //     return;
+  //   } else {
+  //     result--;
+  //     this.setState({ count: result })
+  //   }
+  // }
 
   incrementCount() {
     this.setState({
@@ -27,7 +46,7 @@ class App extends React.Component {
 
   generateAlert = () => {
     alert("You can't do that! Come to the dark side, we have cookies!");
-  };
+  }
 
   render() {
     return (
