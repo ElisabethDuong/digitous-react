0783fefd2b318c65ba182ae09afee16635abf519
ElisabethDuong <elisabeth.duong@gmail.com>
2021-02-02 15:54:08 +0100

    add list pay.jsx in bakery folder

---
+++ bakery/src/components/Pay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class PAy extends React.Component {
+    render() {
+        return (
+            <div>
+                <Pay />
+            </div>
+        )
+    }
+}
